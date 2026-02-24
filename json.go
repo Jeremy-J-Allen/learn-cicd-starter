1b15c20ebcb4cb437e8134c2650cbcce3a88daf4
Jeremy-J-Allen <finchfighter@gmail.com>
2026-02-23 20:33:28 -0700

    updated files

--- json.go
+++ json.go
@@ -1,38 +1,38 @@
 package main
 
 import (
-    "encoding/json"
-    "log"
-    "net/http"
+	"encoding/json"
+	"log"
+	"net/http"
 )
 
 func respondWithError(w http.ResponseWriter, code int, msg string, logErr error) {
-        if logErr != nil {
-            log.Println(logErr)
+	if logErr != nil {
+		log.Println(logErr)
 	}
 	if code > 499 {
-	    log.Printf("Responding with 5XX error: %s", msg)
+		log.Printf("Responding with 5XX error: %s", msg)
 	}
 	type errorResponse struct {
-	    Error string `json:"error"`
+		Error string `json:"error"`
 	}
 	respondWithJSON(w, code, errorResponse{
-	    Error: msg,
-        })
+		Error: msg,
+	})
 }
 
 func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	w.Header().Set("Content-Type", "application/json")
 	dat, err := json.Marshal(payload)
 	if err != nil {
-	    log.Printf("Error marshalling JSON: %s", err)
-	    w.WriteHeader(500)
-	    return
+		log.Printf("Error marshalling JSON: %s", err)
+		w.WriteHeader(500)
+		return
 	}
 	w.WriteHeader(code)
 	_, err = w.Write(dat)
 	if err != nil {
-	    log.Printf("Critical error writing response: %s", err)
-        }
+		log.Printf("Critical error writing response: %s", err)
+	}
 }
 
